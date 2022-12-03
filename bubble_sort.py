67c704e396e39b2536f1b7f6a2e59fa51a9dc30f
NightCatrin <ektant9@gmail.com>
2022-12-03 13:16:43 +0700

    Сортировка пузырьком

---
+++ bubble_sort.py
@@ -0,0 +1,15 @@
+import random
+
+num = 100
+array = []
+for i in range(num):
+    array.append(i)
+random.shuffle(array)
+
+print(array)
+
+for i in range(len(array)-1):
+    for j in range(len(array)-1-i):
+        if array[j] > array[j+1]:
+            array[j], array[j+1] = array[j+1], array[j]
+print(array)
