c4f77e7a5925cc4bf2cf41e82ed7500544a40e13
NightCatrin <ektant9@gmail.com>
2023-01-06 23:11:29 +0700

    Сортировка вставками

---
+++ insertion_sort.py
@@ -0,0 +1,18 @@
+import random
+
+num = 100
+array = []
+for i in range(num):
+    array.append(i)
+random.shuffle(array)
+
+for i in range(1, len(array)):
+    current = array[i]
+    prev_cur = i - 1
+
+    while prev_cur >= 0 and array[prev_cur] > current:
+        array[prev_cur + 1] = array[prev_cur]
+        prev_cur -= 1
+        array[prev_cur+1] = current
+
+print(array)
